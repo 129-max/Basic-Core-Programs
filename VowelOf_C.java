39582792534d51d1b169a876737d2301837782d5
Mohit Raj <rajmohit.129@gmail.com>
2022-07-08 08:22:18 +0530

    to check vowel and consonent

---
+++ VowelOf_C.java
@@ -0,0 +1,12 @@
+package com.example.bridgelabz;
+
+public class VowelOf_C {
+
+    public static void main(String[] args) {
+
+        Day5logic vowel = new Day5logic();
+        System.out.print("Enter the alphabate : ");
+        char alphabate = vowel.getCharValue();
+        vowel.checkAlphabate(alphabate);
+    }
+}
